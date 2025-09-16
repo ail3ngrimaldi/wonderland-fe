1db227a4993db55e362fbe1d3c4a5ff31859931e
ail3n <ailenrgrimaldi@gmail.com>
2025-09-16 02:56:03 -0300

    first commit - initial template

---
+++ blockchain-challenge/src/App.tsx
@@ -0,0 +1,13 @@
+import { Container, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h4" align="center" sx={{ mt: 4 }}>
+        Blockchain Challenge
+      </Typography>
+    </Container>
+  );
+}
+
+export default App;
